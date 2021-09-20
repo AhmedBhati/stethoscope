bff32f94b87f0c515c98e5102aaee1ecc92b761c
AhmedBhati <48443838+AhmedBhati@users.noreply.github.com>
2021-09-20 19:05:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 def index():
     return 'check the noise reduction'
 
-@app.route('/update', methods=['POST'])
-def check():
-    data1 = request.get_json()
-    heart_beat = data1['heart_beat']
-    doc_id = data1['doc_id']
-    rate = 44100
-    reduced_noise = nr.reduce_noise(y = heart_beat, sr=rate, n_std_thresh_stationary=1.5,stationary=False, freq_mask_smooth_hz=500,n_fft=2048)
-    k = str(reduced_noise.tolist())
-    return jsonify({'result': k})
+#@app.route('/update', methods=['POST'])
+#def check():
+#    data1 = request.get_json()
+#    heart_beat = data1['heart_beat']
+#    doc_id = data1['doc_id']
+#    rate = 44100
+#    reduced_noise = nr.reduce_noise(y = heart_beat, sr=rate, n_std_thresh_stationary=1.5,stationary=False, freq_mask_smooth_hz=500,n_fft=2048)
+#    k = str(reduced_noise.tolist())
+#    return jsonify({'result': k})
